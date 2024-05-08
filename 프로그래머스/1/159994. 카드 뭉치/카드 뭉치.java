cf4e75c3f780b0e96fd3ff270971bef07ed36cf8
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-09 00:34:49 +0900

    [level 1] Title: 카드 뭉치, Time: 0.50 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,21 +1,13 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class Solution {
 	public static String solution(String[] cards1, String[] cards2, String[] goal) {
-		ArrayDeque<String> queGoal = new ArrayDeque<String>();
-		ArrayDeque<String> queC1 = new ArrayDeque<String>();
-		ArrayDeque<String> queC2 = new ArrayDeque<String>();
+		ArrayDeque<String> queGoal = new ArrayDeque<String>(Arrays.asList(goal));
+		ArrayDeque<String> queC1 = new ArrayDeque<String>(Arrays.asList(cards1));
+		ArrayDeque<String> queC2 = new ArrayDeque<String>(Arrays.asList(cards2));
 
-		for (int i = 0; i < goal.length; i++) {
-			queGoal.addLast(goal[i]);
-
-			if (i < cards1.length)
-				queC1.addLast(cards1[i]);
-			if (i < cards2.length)
-				queC2.addLast(cards2[i]);
-		}
-
-		for (int i = 0; i < goal.length; i++) {
+		while (!queGoal.isEmpty()) {
 			if ((!queC1.isEmpty() && queC1.peekFirst().equals(queGoal.peekFirst()))) {
 				queC1.pollFirst();
 				queGoal.pollFirst();
