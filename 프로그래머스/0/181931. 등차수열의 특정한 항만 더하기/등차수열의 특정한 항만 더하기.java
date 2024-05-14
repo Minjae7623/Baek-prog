683ee8f7c8098c2fe7bc7cd959fc9b04812ae04b
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-14 18:08:26 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -3,8 +3,8 @@ class Solution {
         int j = a;
         int answer = 0;
 
-        for(int i =0; i < included.length; i++){
-            if(included[i])
+        for (boolean i : included) {
+            if (i)
                 answer += j;
             j += d;
         }
