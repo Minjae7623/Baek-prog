1e1ef405c824e407a0dfb5bd2d6b6630aa0f194e
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-14 22:05:32 +0900

    [level 2] Title: 오픈채팅방, Time: 112.04 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Solution {
+    public static String[] solution(String[] record) {
+        ArrayList<String> ment = new ArrayList<>();
+        HashMap<String, String> idMap = new HashMap<>();
+        String[] answer;
+        String[] keyward;
+
+        for (String cmd : record) {
+            keyward = cmd.split(" ");
+
+            if (keyward[0].equals("Enter")) {
+                idMap.put(keyward[1], keyward[2]);
+            } else if (keyward[0].equals("Change"))
+                idMap.put(keyward[1], keyward[2]);
+        }
+
+        for (String cmd : record) {
+            keyward = cmd.split(" ");
+
+            if (keyward[0].equals("Enter"))
+                ment.add(idMap.get(keyward[1]) + "님이 들어왔습니다.");
+            else if (keyward[0].equals("Leave"))
+                ment.add(idMap.get(keyward[1]) + "님이 나갔습니다.");
+        }
+        answer = ment.toArray(new String[0]);
+
+        return answer;
+    }
+}
