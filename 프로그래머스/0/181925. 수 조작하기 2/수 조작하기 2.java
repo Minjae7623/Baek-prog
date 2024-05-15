404c737cecdada6760f75d879a92c0f7ed77117f
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-16 01:43:43 +0900

    [level 0] Title: 수 조작하기 2, Time: 5.88 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -6,14 +6,10 @@ class Solution {
             int j = i - 1;
 
             if (numLog[j] + 1 == numLog[i]) answer.append("w");
-            if (numLog[j] - 1 == numLog[i]) answer.append("s");
-            if (numLog[j] + 10 == numLog[i]) answer.append("d");
-            if (numLog[j] - 10 == numLog[i]) answer.append("a");
+            else if (numLog[j] - 1 == numLog[i]) answer.append("s");
+            else if (numLog[j] + 10 == numLog[i]) answer.append("d");
+            else if (numLog[j] - 10 == numLog[i]) answer.append("a");
         }
         return answer.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(solution(new int[] {0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1}));
     }
 }
