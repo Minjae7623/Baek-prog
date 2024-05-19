dcf90a5c78ca07d01d6b07f9a72858970cef3c37
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-20 00:21:56 +0900

    [level 0] Title: 배열 만들기 2, Time: 7.15 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -4,29 +4,20 @@ class Solution {
     public int[] solution(int l, int r) {
         int[] answer = {};
         ArrayList<Integer> list = new ArrayList<>();
-        boolean chk = false;
+        boolean chk = true;
 
         for (int i = l; i <= r; i++) {
-            String s = String.valueOf(i);
-            for (char c : s.toCharArray()) {
-                if (c == '5' || c == '0')
-                    chk = true;
-                else
+            for (char c : String.valueOf(i).toCharArray()) {
+                if (c != '5' && c != '0') {
                     chk = false;
-                if(!chk)
                     break;
+                } else
+                    chk = true;
             }
             if (chk) {
-                list.add(Integer.parseInt(s));
-                chk = false;
+                list.add(i);
             }
         }
-
-        if (list.isEmpty())
-            list.add(-1);
-
-        answer = list.stream().mapToInt(i -> i).toArray();
-
-        return answer;
+        return answer = list.isEmpty() ? new int[]{-1} : list.stream().mapToInt(i -> i).toArray();
     }
 }
