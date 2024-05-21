7c8963a0428aca8cef120d21601badaa294166f3
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-22 00:57:37 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(String number) {
+        int answer = 0;
+
+        for(char c : number.toCharArray())
+            answer += Character.getNumericValue(c);
+
+        return answer % 9;
+    }
+}
