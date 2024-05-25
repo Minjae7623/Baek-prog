fed29cbe1855259f09aa1d535ca66435550af5c7
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-25 23:26:10 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,8 +1,5 @@
 class Solution {
     public String solution(String my_string, int n) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(my_string.substring(my_string.length() - n, my_string.length()));
-        
-        return sb.toString();
+        return my_string.substring(my_string.length() - n, my_string.length());
     }
 }
