beba9fbb92c517abc885dea6c9593276d70c3c0c
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-05-30 19:24:05 +0900

    [level 0] Title: 글자 지우기, Time: 4.41 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -4,9 +4,9 @@ import java.util.Collections;
 class Solution {
     public static String solution(String my_string, int[] indices) {
         StringBuilder answer = new StringBuilder(my_string);
-        int [] arr = Arrays.stream(indices).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer :: intValue).toArray();
+        indices = Arrays.stream(indices).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer :: intValue).toArray();
 
-        for(int i : arr)
+        for(int i : indices)
             answer.deleteCharAt(i);
 
         return answer.toString();
