09d87bd9193612a4e5d401de768d0e75ef106445
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2024-11-27 01:26:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,12 @@
+int main()
+{
+	int num1 = 0, num2 = 0;
+	scanf("%d%d", &num1, &num2);
+
+	printf("%d \n", num1 + num2);
+	printf("%d \n", num1 - num2);
+	printf("%d \n", num1 * num2);
+	printf("%d \n", num1 / num2);
+	printf("%d \n", num1 % num2);
+	return 0;
+}
