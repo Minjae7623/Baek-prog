4ad6bf09ca41fad48fcd1984323d5efbca2b452a
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2025-02-17 23:48:10 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -13,24 +13,11 @@ enum CASTE
 int solution(int hp) {
     int answer = 0;
 
-    while (hp) 
-    {
-        if (hp >= GENERAL)          //5~1000
-        {
-            answer += (hp / GENERAL);
-            hp %= GENERAL;
-        }
-        else if (hp >= SOLDIER)     //4~3
-        {
-            answer += (hp / SOLDIER);
-            hp %= SOLDIER;
-        }
-        else if (hp >= WORKER)     //2~1
-        {
-            answer += hp;
-            hp = 0;
-        }
-    }
-
+    answer += (hp / GENERAL);
+    hp %= GENERAL;
+    answer += (hp / SOLDIER);
+    hp %= SOLDIER;          
+    answer += hp;
+   
     return answer;
 }
