f311457e8f83fb7a8258b8de21f397f65efd85ea
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2025-03-11 10:41:14 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+// 집합의 조합: |A|*|B| 
+
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     unordered_map<string, int> cloType;
@@ -13,10 +15,12 @@ int solution(vector<vector<string>> clothes) {
         ++cloType[clotheVec[1]];
     }
 
+    // 0개인 경우를 고려하여 +1
     for (const auto& count : cloType)
     {
         answer *= (count.second + 1);
     }
 
+    // 모든 종류가 0개인 경우를 제외하여 -1
     return answer - 1;
 }
