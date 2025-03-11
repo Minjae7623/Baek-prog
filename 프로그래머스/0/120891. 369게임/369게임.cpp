3476a072b2bd450aa1b602274f0a2ee49b9c5870
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2025-03-12 00:34:08 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    string str(to_string(order));
+
+    for (char c : str)
+    {
+        if (c == '3' || c == '6' || c == '9')
+            ++answer;
+    }
+
+    return answer;
+}
