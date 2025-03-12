d1e20a8033fe5159dc56363817e91fa9a919b994
MinJae_Lee <149874764+Minjae7623@users.noreply.github.com>
2025-03-12 17:01:04 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/README.md
+++ 프로그래머스/0/181839. 주사위 게임 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 1 - 181839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181839?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181839) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.01 ms
+메모리: 4.12 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 16:45:54
+2025년 03월 12일 17:01:01
 
 ### 문제 설명
 
--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int solution(int a, int b) {
-    if (!(a % 2 == 0) && !(b % 2 == 0)) { return a * a + b * b; }
-    else if (!(a % 2 == 0) || !(b % 2 == 0)) { return 2 * (a + b); }
-    else { return abs(a - b); }
+    if      (a & 1 && b & 1)  return a * a + b * b;
+    else if (a & 1 || b & 1)  return 2 * (a + b); 
+    else                      return abs(a - b);
 }
